12994b472e21bd976617dfbcaed4c3f7a00ccaca
lamongabriel <gabriel-lamon@outlook.com>
2022-12-22 14:07:02 -0300

    header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,5 +1,17 @@
+import Link from 'next/link';
+import styles from './header.module.scss';
+import commonStyles from '../../styles/common.module.scss';
+
 export default function Header(): JSX.Element {
   return (
-
-  )
+    <header className={styles.header}>
+      <div className={commonStyles.container}>
+        <Link href="/">
+          <a>
+            <img src="/logo.png" alt="logo" />
+          </a>
+        </Link>
+      </div>
+    </header>
+  );
 }
